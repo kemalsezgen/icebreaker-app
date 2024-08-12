f066f1b6b760c075098d49284f7f9f73433ea97e
Kemal Sezgen <kemalsezgen1@gmail.com>
2024-08-12 22:13:21 +0300

    QuestionType constant added

---
+++ frontend/src/constants/QuestionTypes.js
@@ -0,0 +1,26 @@
+export const questionTypes = [
+  {
+    value: "MORAL_DILEMMAS",
+    label: "MORAL DILEMMAS",
+  },
+  {
+    value: "PREFERENCES",
+    label: "PREFERENCES",
+  },
+  {
+    value: "HYPOTHETICAL_SCENARIOS",
+    label: "HYPOTHETICAL SCENARIOS",
+  },
+  {
+    value: "PERSONALITY_REFLECTIONS",
+    label: "PERSONALITY REFLECTIONS",
+  },
+  {
+    value: "SOCIAL_ISSUES",
+    label: "SOCIAL ISSUES",
+  },
+  {
+    value: "MIX",
+    label: "ALL",
+  },
+];
